a20c623829b2291ab9e6a8654aa6e50666bd9134
qngapparat <jakob.wallnoefer@outlook.com>
2020-12-09 15:37:07 +0100

    merge index, aln, sampe

--- functions/bwa-aln/index.js
+++
@@ -1,5 +0,0 @@
-exports.handler = ({ s3WorkingDir }, context) => {
-  context.succeed({
-    s3WorkingDir: s3WorkingDir
-  })
-}
--- functions/bwa-sampe/index.js
+++
@@ -1,5 +0,0 @@
-exports.handler = (event, context) => {
-  context.succeed({
-    samPath: "sam/path/to/sam"
-  })
-}

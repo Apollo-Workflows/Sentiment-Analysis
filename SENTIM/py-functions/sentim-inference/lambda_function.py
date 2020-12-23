03403261918b4a34c7f4abd9c51de24b9496062f
qngapparat <jakob.wallnoefer@outlook.com>
2020-12-23 11:57:57 +0100

    sentim: Tensorflow Lite layer

--- SENTIM/py-functions/sentim-inference/lambda_function.py
+++ SENTIM/py-functions/sentim-inference/lambda_function.py
@@ -1,6 +1,8 @@
 import sys
 
+# Uses this TF Lite Layer: https://jak-sentim-bucket.s3.eu-central-1.amazonaws.com/tflite-for-amazon-linux-env.zip
+
 def lambda_handler(event, context): 
-  sys.path.append('/opt') # make Python see it
+  sys.path.append('/opt/site-packages') # make Python see it
   import tflite_runtime.interpreter as tflite
   print("tflite imported")

79470568f70c8fb1dfe0320a255bdee920729427
qngapparat <jakob.wallnoefer@outlook.com>
2020-12-28 11:24:07 +0100

    refactor batching

--- SENTIM/py-functions/sentim-batch/lambda_function.py
+++ SENTIM/py-functions/sentim-batch/lambda_function.py
@@ -1,3 +1,5 @@
+import math
+
 # https://stackoverflow.com/a/1751478
 def batch(arr, n):
     n = max(1, n)
@@ -5,10 +7,14 @@ def batch(arr, n):
 
 def lambda_handler(event, context): 
   all_tweets = event['all_tweets']
-  batch_size = event['batch_size'] # number of tweets per batch
-
+  num_batches = event['num_batches']
+  batch_size = math.ceil(len(all_tweets) / num_batches)
+  
   batched_all_tweets = batch(all_tweets, batch_size)
 
-  return batched_all_tweets
+  keyed = [ { 'tweets': batch } for batch in batched_all_tweets ]
+  
+  batches = { 'batches': keyed }
+  return batches
   
 
